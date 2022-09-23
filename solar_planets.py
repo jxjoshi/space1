d519d184387099abbeddbad9745810ce8f527ecf
Jai Joshi <83172501+jxjoshi@users.noreply.github.com>
2022-09-23 11:41:45 -0500

    Update solar_planets.py

--- solar_planets.py
+++ solar_planets.py
@@ -1,6 +1,8 @@
 import imghdr
 import cv2
 
+img=cv2.imread("solar-system.jpg")
+
 cv2.putText(img,
             "Sun",
             (20,300),
@@ -67,3 +69,7 @@ cv2.putText(img,
             0.5,
             (255,255,255)
             )
+
+cv2.imshow("output",img)
+cv2.waitKey(0)
+cv2.imwrite("solarsystemwithname.jpg", img)
